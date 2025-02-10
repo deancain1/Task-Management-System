be0998df9b5810c65c7b7e6c833a81c131a15a63
Dean Cain A. Ronquillo <caindeanw@gmail.com>
2025-02-10 20:03:44 +0800

    Add files via upload

---
+++ Presentation/UserInterface/NavigationBarForm.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task_Management_System.Domain.Entities;
+using Task_Management_System.Presentation.AdminInterface;
+using Task_Management_System.Presentation.Modal;
+using Task_Management_System.Services.Contracts;
+
+namespace Task_Management_System.Presentation.UserInterface
+{
+    public partial class NavigationBarForm : Form
+    {
+        public UserEntity? currentUser;
+        public NavigationBarForm()
+        {
+            InitializeComponent();
+            var homeForm = Program.ServiceProvider.GetRequiredService<HomeForm>();
+            LoadForm(MainPanel, homeForm);
+
+        }
+        private void HomeBtn_Click(object sender, EventArgs e)
+        {
+            var homeForm = Program.ServiceProvider.GetRequiredService<HomeForm>();
+            LoadForm(MainPanel, homeForm);
+        }
+        private void TaskBtn_Click(object sender, EventArgs e)
+        {
+            var tasksForm = Program.ServiceProvider.GetRequiredService<TasksForm>();
+            LoadForm(MainPanel, tasksForm);
+        }
+        private void CompletedBtn_Click(object sender, EventArgs e)
+        {
+            var completedTaskForm = Program.ServiceProvider.GetRequiredService<CompletedTaskForm>();
+            LoadForm(MainPanel, completedTaskForm);
+
+        }
+        private void LogoutBtn_Click(object sender, EventArgs e)
+        {
+            var loginForm = Program.ServiceProvider.GetRequiredService<LoginForm>();
+            loginForm?.Show();
+            this.Close();
+
+        }
+        public void LoadForm(Panel mainPanel, Form newForm)
+        {
+            if (mainPanel.Controls.Count > 0)
+                mainPanel.Controls.Clear();
+                newForm.TopLevel = false;
+                newForm.Dock = DockStyle.Fill;
+
+                mainPanel.Controls.Add(newForm);
+                mainPanel.Tag = newForm;
+                newForm.Show();
+        }
+
+        private void NavigationBarForm_Load(object sender, EventArgs e)
+        {
+            if (currentUser != null)
+            {
+                UserLbl.Text = $"{"Welcome back! " + currentUser.FirstName} ";
+
+            }
+        }
+    }
+}
---
+++ Presentation/UserInterface/TasksForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task_Management_System.Data_Connectivity.Context;
+using Task_Management_System.Domain.Entities;
+using Task_Management_System.Presentation.User_Control;
+using Task_Management_System.Services;
+using Task_Management_System.Services.Contracts;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Task_Management_System.Presentation.UserInterface
+{
+    public partial class TasksForm : Form
+    {
+        private readonly ITaskServices _taskServices;
+     
+     
+        public TasksForm(ITaskServices taskServices  )
+        {
+
+            InitializeComponent();
+            this._taskServices = taskServices;
+           
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            LoadTasks();
+        }
+        public async void LoadTasks()
+        {
+            try
+            {
+               FLPTasks.Controls.Clear();
+                var tasks = await _taskServices.GetTasksForUserAsync();
+
+                // Populate the UI with tasks
+                foreach (var task in tasks)
+                {
+
+                    DisplayTaskControl taskControl = new DisplayTaskControl(_taskServices);
+                    taskControl.SetTask(task);
+
+                    FLPTasks.Controls.Add(taskControl);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load tasks: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
