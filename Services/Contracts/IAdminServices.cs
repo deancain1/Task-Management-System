7acb0191809e0aa2950fb945d0888e7afdd4f5b0
Dean Cain A. Ronquillo <caindeanw@gmail.com>
2025-02-12 18:43:37 +0800

    Add files via upload

---
+++ Services/Contracts/IAdminServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_Management_System.Domain.Entities;
+
+namespace Task_Management_System.Services.Contracts
+{
+    public interface IAdminServices
+    {
+        Task<AdminEntity?> GetAdminByEmailAsync(string email);
+        Task<List<AdminEntity>> GetAllAdminAsync();
+        Task<AdminEntity?> GetAdminByIDAsync(int adminID);
+        Task<AdminEntity?> GetUserByEmailAsync(string email);
+        Task<bool> DeleteAdminAsync(string email);
+        Task UpdateAdminAccountAsync(AdminEntity adminEntity);
+    }
+}
