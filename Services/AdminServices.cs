49d87012d02c981bfe681debe0516a867edabc78
Dean Cain A. Ronquillo <caindeanw@gmail.com>
2025-02-13 08:46:31 +0800

    Add files via upload

---
+++ Services/AdminServices.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_Management_System.Data_Connectivity.Context;
+using Task_Management_System.Domain.Entities;
+
+using Task_Management_System.Services.Contracts;
+
+namespace Task_Management_System.Services
+{
+    public class AdminServices : IAdminServices
+    {
+        private readonly DbContextOptions<TMSDbContext> _dbContextOptions;
+        public AdminServices(DbContextOptions<TMSDbContext> dbContextOptions)
+        {
+            _dbContextOptions = dbContextOptions;
+        }
+
+        public async Task<AdminEntity?> GetAdminByEmailAsync(string email)
+        {
+            using (var dbContex = new TMSDbContext(_dbContextOptions))
+            {
+                return await dbContex.Admin
+                             .Where(a => a.Email == email)
+                             .FirstOrDefaultAsync();
+            }
+        }
+
+        public async Task<AdminEntity?> GetAdminByIDAsync(int adminID)
+        {
+            using (var dbContex = new TMSDbContext(_dbContextOptions))
+            {
+                return await dbContex.Admin.FirstOrDefaultAsync(a => a.AdminID == adminID);
+            }
+        }
+
+        public async Task<List<AdminEntity>> GetAllAdminAsync()
+        {
+            using (var dbContex = new TMSDbContext(_dbContextOptions))
+            {
+                return await dbContex.Admin.ToListAsync();
+            }
+        }
+
+        public async Task<AdminEntity?> GetUserByEmailAsync(string email)
+        {
+            using (var dbContex = new TMSDbContext(_dbContextOptions))
+            {
+                return await dbContex.Admin
+                            .Where(a => a.Email == email)
+                            .FirstOrDefaultAsync();
+            }
+        }
+        public async Task UpdateAdminAccountAsync(AdminEntity adminEntity)
+        {
+            using (var dbContex = new TMSDbContext(_dbContextOptions))
+            {
+                var existingAdmin = await dbContex.Admin.FirstOrDefaultAsync(admin => admin.AdminID == admin.AdminID);
+                if (existingAdmin != null)
+                {
+                    existingAdmin.LastName = adminEntity.LastName;
+                    existingAdmin.FirstName = adminEntity.FirstName;
+                    existingAdmin.MiddleName = adminEntity.MiddleName;
+                    existingAdmin.Email = adminEntity.Email;
+
+                    await dbContex.SaveChangesAsync();
+                }
+            }
+        }
+        public async Task<bool> DeleteAdminAsync(string email)
+        {
+            using (var dbContex = new TMSDbContext(_dbContextOptions))
+            {
+                var admin = await dbContex.Admin.
+                    FirstOrDefaultAsync(admin => email == admin.Email);
+                if (admin == null)
+                {
+                    return false;
+                }
+
+                dbContex.Admin.Remove(admin);
+                await dbContex.SaveChangesAsync();
+                return true;
+            }
+        }
+    }
+}
---
+++ Services/UserServices.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task_Management_System.Data_Connectivity.Context;
+using Task_Management_System.Domain.DTO;
+using Task_Management_System.Domain.Entities;
+using Task_Management_System.Helpers;
+using Task_Management_System.Services.Contracts;
+
+namespace Task_Management_System.Services
+{
+    public class UserServices : IUserServices
+    {
+        private readonly DbContextOptions<TMSDbContext> _dbContextOptions;
+        public UserServices(DbContextOptions<TMSDbContext> dbContextOptions)
+        {
+
+            _dbContextOptions = dbContextOptions;
+
+        }
+
+        public async Task<bool> DeleteUserAsync(string email)
+        {
+            using (var dbContext = new TMSDbContext(_dbContextOptions))
+            {
+                var user = await dbContext.Users.
+                    FirstOrDefaultAsync(users => email == users.Email);
+                if (user == null)
+                {
+                    return false;
+                }
+
+                dbContext.Users.Remove(user);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public async Task<List<UserEntity>> GetAllUserNamesAsync()
+        {
+            using (var dbContext = new TMSDbContext(_dbContextOptions))
+            {
+                return await dbContext.Users
+                                           .Select(user => new UserEntity
+                                           {
+                                               UserID = user.UserID,
+                                               FirstName = user.FirstName,
+                                               MiddleName = user.MiddleName,
+                                               LastName = user.LastName
+                                           })
+                                           .ToListAsync();
+            }
+        }
+
+        public async Task<List<UserEntity>> GetAllUsersAsync()
+        {
+            using (var dbContext = new TMSDbContext(_dbContextOptions))
+            {
+                return await dbContext.Users.ToListAsync();
+            }
+        }
+
+        public async Task<UserEntity?> GetUserByEmailAsync(string email)
+        {
+            try
+            {
+                using (var dbContext = new TMSDbContext(_dbContextOptions))
+                {
+                    return await dbContext.Users
+                   .FirstOrDefaultAsync(u => u.Email == email);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error retrieving users email.{ex.Message} ", ex);
+            }
+        }
+
+        public async Task<UserEntity?> GetUserByIdAsync(int userID)
+        {
+            using (var dbContext = new TMSDbContext(_dbContextOptions))
+            {
+                return await dbContext.Users.FirstOrDefaultAsync(a => a.UserID == userID);
+            }
+        }
+
+        public async Task<UserEntity?> GetUserInfoAsync()
+        {
+            using (var dbContext = new TMSDbContext(_dbContextOptions))
+            {
+                int loggedInUserId = UserSession.UserID;
+
+                return await dbContext.Users
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(user => user.UserID == loggedInUserId);
+                   
+
+            }
+        }
+
+        public async Task UpdateAccountUserWithPasswordAsync(UserDTO userDTO)
+        {
+           using (var dbContext = new TMSDbContext(_dbContextOptions))
+            {
+                try
+                {
+
+                    var existingUser = await dbContext.Users
+                        .SingleOrDefaultAsync(u => u.Email.ToLower() == userDTO.Email.ToLower());
+
+                    if (existingUser == null)
+                    {
+                        throw new InvalidOperationException($"No user found with the email '{userDTO.Email}'.");
+                    }
+
+
+                    existingUser.FirstName = userDTO.FirstName;
+                    existingUser.LastName = userDTO.LastName;
+                    existingUser.MiddleName = userDTO.MiddleName;
+                    existingUser.SecurityQuestions = userDTO.SecurityQuestions;
+                    existingUser.Answer = userDTO.Answer;
+                    existingUser.Role = userDTO.Role;
+
+
+                    if (!string.IsNullOrEmpty(userDTO.Password))
+                    {
+                        PasswordHashAndSalt.CreatePasswordHash(userDTO.Password, out byte[] passwordHash, out byte[] passwordSalt);
+                        existingUser.PasswordHash = passwordHash;
+                        existingUser.PasswordSalt = passwordSalt;
+                    }
+
+
+                    if (userDTO.ProfilePicture != null)
+                    {
+                        existingUser.ProfilePicture = userDTO.ProfilePicture;
+                    }
+
+                    dbContext.Users.Update(existingUser);
+                    await dbContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error Updating User: {ex.Message}", ex.InnerException ?? ex);
+                }
+            }
+        }
+      
+        public async Task UpdateUserAccountAsync(UserEntity userEntity)
+        {
+            using (var dbContext = new TMSDbContext(_dbContextOptions))
+            {
+                var existingUser = await dbContext.Users.FirstOrDefaultAsync(user => user.UserID == user.UserID);
+                if (existingUser != null)
+                {
+                    existingUser.LastName = userEntity.LastName;
+                    existingUser.FirstName = userEntity.FirstName;
+                    existingUser.MiddleName = userEntity.MiddleName;
+                    existingUser.Email = userEntity.Email;
+                    existingUser.ProfilePicture = userEntity.ProfilePicture;
+
+
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
