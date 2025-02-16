45f2d513917e79e7eaaa1d8b7359357b25afcadd
Dean Cain A. Ronquillo <caindeanw@gmail.com>
2025-02-16 22:11:07 +0800

    Add files via upload

---
+++ Helpers/LoadDataEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_System.Helpers
+{
+    public class LoadDataEventHandler
+    {
+        public event EventHandler? TaskCreated;
+        public event EventHandler? UserCreated;
+        public event EventHandler? TaskCompleted;
+
+
+        public void OnTaskCreated()
+        {
+            TaskCreated?.Invoke(this, EventArgs.Empty);
+        }
+        public void OnUserCreated()
+        {
+            UserCreated?.Invoke(this, EventArgs.Empty);
+        }
+        public void OnTaskCompleted()
+        {
+            TaskCompleted?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Helpers/StatusUpdater.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_System.Helpers
+{
+    public class StatusUpdater
+    {
+     
+        public static void UpdateStatusLabel(Label statusLabel, string selectedStatus, bool updateLabel)
+        {
+            if (updateLabel)
+            {
+               
+                if (statusLabel.Text != selectedStatus)
+                {
+                    switch (selectedStatus)
+                    {
+                        case "Incomplete":
+                            statusLabel.Text = "Incomplete";  
+                            statusLabel.ForeColor = Color.Red; 
+                            break;
+                        case "In Progress":
+                            statusLabel.Text = "In Progress";  
+                            statusLabel.ForeColor = Color.MediumSlateBlue; 
+                            break;
+                        case "Completed":
+                            statusLabel.Text = "Completed";  
+                            statusLabel.ForeColor = Color.Green; 
+                            break;
+                        default:
+                            statusLabel.Text = "Unknown Status";  
+                            statusLabel.ForeColor = Color.Black; 
+                            break;
+                    }
+                }
+            }
+        }
+
+        public static void SetInitialStatusColor(Label statusLabel, string initialStatus)
+        {
+         
+            switch (initialStatus)
+            {
+                case "Incomplete":
+                    statusLabel.ForeColor = Color.Red;  
+                    break;
+                case "In Progress":
+                    statusLabel.ForeColor = Color.MediumSlateBlue;  
+                    break;
+                case "Completed":
+                    statusLabel.ForeColor = Color.Green;  
+                    break;
+                default:
+                    statusLabel.ForeColor = Color.Black;  
+                    break;
+            }
+        }
+    }
+}
+
