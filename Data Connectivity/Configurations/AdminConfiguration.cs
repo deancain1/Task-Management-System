02a7aa2201f8600d7f89283b3294f2a4ed1cb627
Dean Cain A. Ronquillo <caindeanw@gmail.com>
2025-02-16 22:09:09 +0800

    Add files via upload

---
+++ Data Connectivity/Configurations/AdminConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using Task_Management_System.Domain.Entities;
+
+namespace Task_Management_System.Data_Connectivity.Configurations
+{
+    public class AdminConfiguration : IEntityTypeConfiguration<AdminEntity>
+    {
+        public void Configure(EntityTypeBuilder<AdminEntity> builder)
+        {
+            
+            builder.HasKey(a => a.AdminID);
+
+         
+            builder.Property(a => a.LastName)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(a => a.FirstName)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(a => a.MiddleName)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(a => a.Email)
+                   .IsRequired()
+                   .HasMaxLength(255);
+
+            builder.Property(a => a.SecurityQuestions)
+          .IsRequired()
+          .HasMaxLength(50);
+
+            builder.Property(a => a.Answer)
+           .IsRequired()
+           .HasMaxLength(50);
+
+
+            builder.Property(a => a.PasswordHash)
+            .IsRequired()
+            .HasColumnType("VARBINARY(MAX)"); 
+            builder.Property(a => a.PasswordSalt)
+           .IsRequired()
+            .HasColumnType("VARBINARY(MAX)"); 
+
+
+        }
+    }
+}
---
+++ Data Connectivity/Configurations/UserConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Task_Management_System.Domain.Entities;
+
+namespace Task_Management_System.Data_Connectivity.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.HasKey(a => a.UserID);
+
+
+
+            builder.Property(a => a.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(a => a.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(a => a.MiddleName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(a => a.Email)
+             .IsRequired()
+             .HasMaxLength(255);
+
+            builder.Property(a => a.Username)
+           .IsRequired()
+           .HasMaxLength(255);
+
+            builder.Property(a => a.Department)
+           .IsRequired()
+           .HasMaxLength(255);
+
+            builder.Property(a => a.Role)
+            .IsRequired()
+            .HasMaxLength(50);
+
+            builder.Property(a => a.SecurityQuestions)
+           .IsRequired()
+           .HasMaxLength(50);
+
+            builder.Property(a => a.Answer)
+           .IsRequired()
+           .HasMaxLength(50);
+
+            builder.HasMany(a => a.userRoleEntities)
+                .WithOne(ur => ur.User)
+                .HasForeignKey(ur => ur.UserID);
+
+
+
+
+
+        }
+    }
+}
