98ffcefa32e0f0fde609ee29c704d0e083a7ef9b
Dean Cain A. Ronquillo <caindeanw@gmail.com>
2025-02-16 22:15:05 +0800

    Add files via upload

---
+++ AdminSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_System
+{
+    public static class AdminSession
+    {
+        public static int AdminID { get; set; } = 0;
+        public static string Email { get; set; } = string.Empty;
+       
+    }
+}
